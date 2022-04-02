5107814413e81fa9fc309762f787803a5257081d
Omar-macbook <omar987@hotmail.co.uk>
2022-04-02 10:39:55 +0100

    DB connection log

--- server/index.js
+++ server/index.js
@@ -8,10 +8,17 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("DB Connection Successful");
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
 
 const server = app.listen(process.env.PORT, () => {
 	console.log(`Server started on port: ${process.env.PORT}`);
