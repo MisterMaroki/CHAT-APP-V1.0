0355ee9efefcce3b69b9dd0957e7357430eb64f9
Omar-macbook <omar987@hotmail.co.uk>
2022-04-02 10:55:46 +0100

    setup initial routes

--- public/src/App.js
+++ public/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
 export default function App() {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
