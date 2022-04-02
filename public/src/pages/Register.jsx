6aad85d23c0139756a9dcd4072151ea91d02a0b1
Omar-macbook <omar987@hotmail.co.uk>
2022-04-02 12:40:05 +0100

    styeld button

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -92,6 +92,20 @@ const FormContainer = styled.div`
 				outline: none;
 			}
 		}
+		button {
+			background-color: #997af0;
+			color: white;
+			padding: 1rem 2rem;
+			border: none;
+			font-weight: bold;
+			cursor: pointer;
+			border-radius: 0.4rem;
+			font-size: 1rem;
+			text-transform: uppercase;
+			&:hover {
+				background-color: #4e0eff;
+			}
+		}
 	}
 `;
 
