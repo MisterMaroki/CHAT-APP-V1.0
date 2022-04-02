1126fc5c54a47486fda14b70a13494961279ffe1
Omar-macbook <omar987@hotmail.co.uk>
2022-04-02 10:37:25 +0100

    mongoose.connect

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
 const server = app.listen(process.env.PORT, () => {
 	console.log(`Server started on port: ${process.env.PORT}`);
 });
