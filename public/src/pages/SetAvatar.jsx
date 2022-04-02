3ab68f85bbf5d839ccc7655c0bfdfc391a2bed40
Omar-macbook <omar987@hotmail.co.uk>
2022-04-02 18:06:20 +0100

    setAvatar route init and connect to multiavatar.com

---
+++ public/src/pages/SetAvatar.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import loader from "../assets/loader.gif";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { setAvatarRoute } from "../utils/APIRoutes";
+
+export default function SetAvatar() {
+	const api = "https://api.com.multiavatar.com/45678945";
+	const navigate = useNavigate();
+	return (
+		<>
+			<Container>setAvatar</Container>
+			<ToastContainer />
+		</>
+	);
+}
+const Container = styled.div``;
