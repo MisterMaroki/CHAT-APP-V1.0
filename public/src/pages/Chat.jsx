22e13924f6b93e872d3180c9f6cc93b5811d567c
Omar-macbook <omar987@hotmail.co.uk>
2022-04-03 11:41:22 +0100

    checks for users avatar image then either go set one or find users contacts

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,19 +1,29 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import{useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes";
 function Chat() {
-	const navigate = useNavigate()
-	const [contacts, setContacts] = useState([])
-	const[currentUser,setCurrentUser]=useState(undefined)
-	useEffect(async ()=>{
+	const navigate = useNavigate();
+	const [contacts, setContacts] = useState([]);
+	const [currentUser, setCurrentUser] = useState(undefined);
+	useEffect(async () => {
 		if (!localStorage.getItem("chat-app-user")) {
 			navigate("/login");
+		} else {
+			setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
 		}
-	})
-	useEffect(async ()=>{
-		if()
-	},[])
+	});
+	useEffect(async () => {
+		if (currentUser) {
+			if (currentUser.isAvatarImageSet) {
+				const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+				setContacts(data.data);
+			} else {
+				navigate("/setAvatar");
+			}
+		}
+	}, [currentUser]);
 	return (
 		<Container>
 			<div className="container"></div>
